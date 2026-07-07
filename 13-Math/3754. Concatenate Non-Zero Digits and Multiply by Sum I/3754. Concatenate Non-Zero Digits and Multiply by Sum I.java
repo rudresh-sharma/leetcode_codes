a7ffb477cef1b67a9a00ab22bcc417d0f5b20d4c
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-07-07 18:56:56 +0530

    [3754] [Time Beats: 99.85%] [Memory Beats: 83.11%] - Leet2Hub

--- 13-Math/3754. Concatenate Non-Zero Digits and Multiply by Sum I/3754. Concatenate Non-Zero Digits and Multiply by Sum I.java
+++ 13-Math/3754. Concatenate Non-Zero Digits and Multiply by Sum I/3754. Concatenate Non-Zero Digits and Multiply by Sum I.java
@@ -1,22 +1,27 @@
 1class Solution {
 2    public long sumAndMultiply(int n) {
-3        StringBuilder num = new StringBuilder();
-4        String s = String.valueOf(n);
-5
-6        if(n==0) return 0L;
-7        long sum = 0;
-8        for(int i=0; i<s.length(); i++){
-9            char ch = s.charAt(i);
-10            if(s.charAt(i) != '0'){
-11                num.append(s.charAt(i));
-12                sum += ch-'0';
-13            }
-14        }
+3        if (n == 0) return 0;
+4
+5        // Reverse n
+6        int rev = 0;
+7        int temp = n;
+8        while (temp > 0) {
+9            rev = rev * 10 + temp % 10;
+10            temp /= 10;
+11        }
+12
+13        long num = 0;
+14        long sum = 0;
 15
-16        long ans = Long.parseLong(num.toString());
-17
-18        ans *= sum;
-19
-20        return ans;
-21    }
-22}
+16        while (rev > 0) {
+17            int digit = rev % 10;
+18            if (digit != 0) {
+19                num = num * 10 + digit;
+20                sum += digit;
+21            }
+22            rev /= 10;
+23        }
+24
+25        return num * sum;
+26    }
+27}
