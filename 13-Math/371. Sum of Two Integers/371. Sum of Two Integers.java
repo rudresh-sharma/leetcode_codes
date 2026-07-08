8f6ec8e2e47b2ef4d9ae6df3f9048589f172d438
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-07-08 10:12:13 +0530

    [371] [Time Beats: 100%] [Memory Beats: 100.00%] - Leet2Hub

---
+++ 13-Math/371. Sum of Two Integers/371. Sum of Two Integers.java
@@ -0,0 +1,23 @@
+//inspired by logical circuit design and boolean algebra;
+//counter - unit of 3;
+//current   incoming  next
+//a b            c    a b
+//0 0            0    0 0
+//0 1            0    0 1
+//1 0            0    1 0
+//0 0            1    0 1
+//0 1            1    1 0
+//1 0            1    0 0
+//a = a&~b&~c + ~a&b&c;
+//b = ~a&b&~c + ~a&~b&c;
+//return a|b since the single number can appear once or twice;
+int singleNumber(vector<int>& nums) {
+    int t = 0, a = 0, b = 0;
+    for(int i = 0; i < nums.size(); ++i) {
+        t = (a&~b&~nums[i]) | (~a&b&nums[i]);
+        b = (~a&b&~nums[i]) | (~a&~b&nums[i]);
+        a = t;
+    }
+    return a | b;
+}
+;
