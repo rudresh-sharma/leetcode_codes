f264bb7c1dff9ebf319b4bbd291d07f3efa664c3
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-07-20 17:48:57 +0530

    [1011] [Time Beats: 67.62%] [Memory Beats: 76.38%] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/1011. Capacity To Ship Packages Within D Days/1011. Capacity To Ship Packages Within D Days.java
@@ -0,0 +1,35 @@
+class Solution {
+    static {
+        for (int i = 0; i < 300; i++) shipWithinDays(new int[0], 1);
+    }
+    public static int shipWithinDays(int[] weights, int days) {
+        int total = 0, heaviest = 0;
+        for (int w : weights) {
+            total += w;
+            heaviest = Math.max(heaviest, w);
+        }
+        int left = Math.max(heaviest, total / days);
+        int right = heaviest * (int) Math.ceil((double) weights.length / days);
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int neededDays = totalDay(weights, mid);
+            if (neededDays <= days) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    private static int totalDay(int[] weights, int capacity) {
+        int days = 1, remaining = capacity;
+        for (int weight : weights) {
+            if (remaining < weight) {
+                days++;
+                remaining = capacity;
+            }
+            remaining -= weight;
+        }
+        return days;
+    }
+}
