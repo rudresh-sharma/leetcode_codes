576113ec20bbc08e52495f01581c3ba31c997fcc
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-07-30 16:15:57 +0530

    [3014] [Time Beats: 100%] [Memory Beats: N/A] - Leet2Hub

---
+++ 04-String-Manipulation/3014. Minimum Number of Pushes to Type Word I/3014. Minimum Number of Pushes to Type Word I.java
@@ -0,0 +1,7 @@
+int LUT[27]={0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 18, 20, 22, 24, 27, 30, 33, 36, 39, 42, 45, 48, 52, 56};
+class Solution {
+public:
+    int minimumPushes(string word) {
+        return LUT[word.size()];
+    }
+};
