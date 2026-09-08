ca7690e531143e41bf76a1c74b798ef13ab0d1be
rudresh-sharma <rudresh.sharma.8602@gmail.com>
2026-09-08 09:49:11 +0530

    [3870] [Time Beats: 83.65%] [Memory Beats: 22.48%] - Leet2Hub

---
+++ 13-Math/3870. Count Commas in Range/3870. Count Commas in Range.java
@@ -0,0 +1,11 @@
+1class Solution {
+2    public int countCommas(int n) {
+3        int count=0;
+4
+5         if(n<1000) return 0;
+6
+7         count = n-999;
+8
+9        return count;
+10    }
+11}
